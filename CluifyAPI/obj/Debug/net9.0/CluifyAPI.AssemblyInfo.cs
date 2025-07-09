63f896201ce03a0c7ec81a58f0a19070480fffc7
Asantae <asantaems@gmail.com>
2025-07-09 19:43:26 -0400

    updating package.json file to build app correctly

--- CluifyAPI/obj/Debug/net9.0/CluifyAPI.AssemblyInfo.cs
+++ CluifyAPI/obj/Debug/net9.0/CluifyAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CluifyAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48cdfefa6eaebf8f9c50dee6c12b57abcb6b9591")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23913b02b1152abc3ffb7616d22a30ce75d6b419")]
 [assembly: System.Reflection.AssemblyProductAttribute("CluifyAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CluifyAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
