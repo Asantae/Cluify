8fc045d8b7efebe68ca2c5721f23ab96a655220b
Asantae <asantaems@gmail.com>
2025-07-10 00:02:08 -0400

    updating cors

--- CluifyAPI/Program.cs
+++ CluifyAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173") // The address of your React app
+                          policy.WithOrigins("http://localhost:5173", "https://Cluify.net")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
