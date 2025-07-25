1f18d448c42c5d7cbef4156f860715350d109e43
Asantae <asantaems@gmail.com>
2025-07-25 15:40:52 -0400

    adding netlify to cors

--- CluifyAPI/Program.cs
+++ CluifyAPI/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddCors(options =>
             policy.WithOrigins(
                 "http://localhost:5173",
                 "https://cluify.net",
-                "https://www.cluify.net"
+                "https://www.cluify.net",
+                "https://cluify.netlify.app"
             )
             .AllowAnyHeader()                     
             .AllowAnyMethod() 
